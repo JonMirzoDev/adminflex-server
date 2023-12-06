22a1e91175b1dd8ad90f0c3969a7074119cf8783
JonMirzoDev <islomjon.swe@gmail.com>
2023-12-06 16:32:09 +0500

    auth controller created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,67 @@
-// authController.js
 const userModel = require('../models/userModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const Joi = require('joi')
 
-const register = (req, res) => {
-  // Validate user data, hash password, and create user
+const registrationSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required()
+})
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+})
+
+const register = async (req, res) => {
+  try {
+    const { error } = registrationSchema.validate(req.body)
+    if (error) {
+      return res.status(400).send(error.details[0].message)
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+    const userId = await userModel.createUser({
+      ...req.body,
+      password: hashedPassword,
+      registrationTime: new Date()
+    })
+
+    res
+      .status(201)
+      .send({ userId: userId, message: 'User created successfully' })
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 }
 
-const login = (req, res) => {
-  // Validate credentials, check user and password, return JWT
+const login = async (req, res) => {
+  try {
+    const { error } = loginSchema.validate(req.body)
+    if (error) {
+      return res.status(400).send(error.details[0].message)
+    }
+
+    const user = await userModel.findUserByEmail(req.body.email)
+    if (!user) {
+      return res.status(401).send('User does not exist')
+    }
+
+    const match = await bcrypt.compare(req.body.password, user.password)
+    if (!match) {
+      return res.status(401).send('Password is incorrect')
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: '1h'
+    })
+
+    res.send({ token: token })
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
 }
 
-// Export controller functions
 module.exports = { register, login }
